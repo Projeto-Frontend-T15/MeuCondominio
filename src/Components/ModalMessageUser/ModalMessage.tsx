a1a4a411fbc0494deb4353c4e889606840775589
Jessica Estevam <jessicafpereira16@gmail.com>
2023-03-07 17:00:52 -0300

    subindo pastas

---
+++ src/Components/ModalMessageUser/ModalMessage.tsx
@@ -0,0 +1,16 @@
+export function MessageModal() {
+  return (
+    <div>
+      <div>
+        <button type="button">X</button>
+        <form>
+          <input type="text" />
+          <button type="submit">Enviar</button>
+        </form>
+        <div>
+          <ul></ul>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/messagePageUser/index.tsx
@@ -0,0 +1,10 @@
+export function MessagePageUser() {
+  return (
+    <li>
+      <h2>Título</h2>
+      <p>Texto</p>
+      <button type="button">Responder</button>
+      <button type="button">Comentários</button>
+    </li>
+  );
+}
