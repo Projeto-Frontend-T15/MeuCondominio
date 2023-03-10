6882d4f9823d989ba596c774685420cb3fbb028e
silviacarvalhodacunha <silviacarvalhodacunha@gmail.com>
2023-03-10 20:56:35 -0300

    feat: register e login

--- src/Components/Form/RegisterForm/SelectCondo/index.tsx
+++ src/Components/Form/RegisterForm/SelectCondo/index.tsx
@@ -1,24 +1,45 @@
-import { useContext } from "react";
+import { useEffect, useState } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
-import { userContext } from "../../../../Contexts/userContext";
+import { toast } from "react-toastify";
+import api from "../../../../services/api";
 
 interface ISelectCondo {
   register: UseFormRegisterReturn<string>;
   error?: any;
 }
 
+interface ICondos {
+  name: string;
+  userId: number;
+  id: number;
+}
+
 export function SelectCondo({ register, error }: ISelectCondo) {
-  const { condo } = useContext(userContext);
+  const [condo, setCondo] = useState<ICondos[]>([]);
+  
+  
+  useEffect(() => {
+    const condos = async () => {
+      try {
+        const response = await api.get("/conds");
+        setCondo(response.data);
+      } catch (error) {
+        toast.error("Algo deu errado ao listar condominios cadastrados");
+      }
+    };
+    condos();
+  }, [])
+
 
   return (
     <fieldset>
       <select {...register}>
         <option value="">Selecionar Condomínio</option>
-        {/* {condo.map((cond) => (
+        {condo.map((cond) => (
           <option key={cond.id} value={cond.id}>
             {cond.name}
           </option>
-        ))} */}
+        ))}
       </select>
       {error && <p>{error.message}</p>}
     </fieldset>
