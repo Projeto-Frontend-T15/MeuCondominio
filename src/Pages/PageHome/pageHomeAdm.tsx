94203ce3eb0bb6665f503758efcfc3a5c711aeaa
pdrocosta <pedro.augusto.costa.99@gmail.comgit>
2023-03-13 23:02:15 -0300

    feature/PageHome

--- src/Pages/PageHome/pageHomeAdm.tsx
+++ src/Pages/PageHome/pageHomeAdm.tsx
@@ -1,33 +1,18 @@
-import { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { RegisterMessages } from "../../Components/Form/MessagensForm";
+import { CondoPage } from "../../Components/Condo/CondoPage";
 import Header from "../../Components/Header/header";
-import { ListConds } from "../../Components/listConds";
-import { ReadMessagens } from "../../Components/MessagensAdm";
-
-import { ModalComents } from "../../Components/ModalComents";
-import ModalCreateCond from "../../Components/ModalCreateCond";
-import { HomeContext } from "../../Contexts/homeContext";
-import { ResidentContext } from "../../Contexts/residentContext";
+import { MaintenancePage } from "../../Components/Maintenance/MaintenancePage";
+import { MessagePage } from "../../Components/Messages/MessagesPage";
 import { MainStyled } from "./styled";
 
 const HomeAdm = () => {
-  const { messages } = useContext(ResidentContext);
-  const { modal, readAllMenssagens, idCond } = useContext(HomeContext);
-
-  useEffect(() => {
-    readAllMenssagens(idCond);
-  }, [idCond]);
   return (
     <>
       <Header home={true} />
       <MainStyled>
         <div className="container_components">
-          <ListConds />
-          {messages ? <ReadMessagens /> : <p>Ainda não existe recados</p>}
-          {modal && <ModalComents />}
-          <RegisterMessages />
-          <ModalCreateCond />
+          <CondoPage />
+          <MessagePage />
+          <MaintenancePage />
         </div>
       </MainStyled>
     </>
